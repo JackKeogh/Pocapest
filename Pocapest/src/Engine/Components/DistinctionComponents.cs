a2e2be5d8fe0118c840441f23ce9e23a43eba1dc
Jack Keogh <keeeogh@gmail.com>
2025-04-02 19:59:05 +0100

    Camera System

---
+++ Pocapest/src/Engine/Components/DistinctionComponents.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pocapest.src.Engine.Components
+{
+	public class PlayerComponent
+	{
+		public string Name { get; set; }
+	}
+}
---
+++ Pocapest/src/Engine/Systems/CameraSystem.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using MonoGame.Extended.ECS;
+using MonoGame.Extended.ECS.Systems;
+using Pocapest.src.Engine.Components;
+using System;
+
+namespace Pocapest.src.Engine.Systems
+{
+	public class CameraSystem : EntityUpdateSystem
+	{
+		private OrthographicCamera camera;
+		private ComponentMapper<PositionComponent> positionMapper;
+		private ComponentMapper<PlayerComponent> playerMapper;
+
+		public CameraSystem(OrthographicCamera camera) :
+			base (Aspect.All(typeof(PositionComponent), typeof(PlayerComponent)))
+		{
+			this.camera = camera;
+		}
+
+		public override void Initialize(IComponentMapperService mapperService)
+		{
+			this.positionMapper = mapperService.GetMapper<PositionComponent>();
+			this.playerMapper = mapperService.GetMapper<PlayerComponent>();
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			foreach (var entity in this.ActiveEntities)
+			{
+				if (playerMapper.Has(entity))
+				{
+					var position = positionMapper.Get(entity);
+
+					camera.Position = new Vector2(position.X, position.Y);
+					break;
+				}
+			}
+		}
+	}
+}
